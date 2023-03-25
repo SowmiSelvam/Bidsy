913a16145ad35e59a091f961072516c8e047113f
Sowmiyanarayan Selvam <55820583+SowmiyanarayanSelvam@users.noreply.github.com>
2023-03-25 07:58:02 -0400

    logout
    have to check if it works

---
+++ src/main/java/com/BidsyJava/Logout.java
@@ -0,0 +1,24 @@
+package main.java.com.BidsyJava;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public Logout() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().removeAttribute("user");
+		response.sendRedirect("login.jsp");
+	}
+
+}
